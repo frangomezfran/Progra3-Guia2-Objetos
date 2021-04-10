584add8144e2293a00d276cb3cef60d09d5674f7
Haag Juan Martin <apetroxtrm@gmail.com>
2021-04-10 15:25:04 -0300

    Se crearon clases Libro Autor
    Libro esta sin nada

---
+++ src/ej1/Autor.java
@@ -0,0 +1,46 @@
+package ej1;
+
+public class Autor {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private Character genero;
+}
+
+    public Autor(String nombre,String apellido, String email, Character genero){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.genero = genero;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Character getGenero() {
+        return this.genero;
+    }
+
+    public void setGenero(Character genero) {
+		this.genero = genero;
+	}
---
+++ src/ej1/Libro.java
@@ -0,0 +1,9 @@
+package ej1;
+
+public class Libro {
+    private String titulo;
+    private double precio;
+    private int stock;
+    
+
+}
